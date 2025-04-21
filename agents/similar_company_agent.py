cefe686fd40674bf2b90accf6f86cc9d1d2ea2a3
dcamodeo1983 <dcamodeo@gmail.com>
2025-04-21 12:28:30 -0400

    Update similar_company_agent.py

--- agents/similar_company_agent.py
+++ agents/similar_company_agent.py
@@ -1,11 +1,29 @@
+# agents/similar_company_agent.py
+
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-class SimilarCompanyFinderAgent:
+class SimilarCompanyAgent:
     def __init__(self, embedder):
-        self.embedder = embedder  # Should be an instance of EmbedderAgent
+        """
+        Args:
+            embedder: An instance of EmbedderAgent used to embed company descriptions.
+        """
+        self.embedder = embedder
 
     def find_similar(self, founder_vec, portfolio_texts: dict, company_to_vcs: dict, top_n=5):
+        """
+        Identify companies in VC portfolios that are similar to the founder's business concept.
+
+        Args:
+            founder_vec: The embedded vector representation of the founder's summary.
+            portfolio_texts: A dict of {company_url: combined website + portfolio text}.
+            company_to_vcs: A dict of {company_url: [list of VC firms that invested]}.
+            top_n: Number of top similar companies to return.
+
+        Returns:
+            A list of dicts containing company_url, similarity_score, and invested_vcs.
+        """
         companies = list(portfolio_texts.keys())
         texts = [portfolio_texts[c] for c in companies]
 
